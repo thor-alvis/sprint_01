2b8c4e8c5f8e2d509e9ef7cfe13d45c758c2ed97
Abi Munoz <abimunozr@gmail.com>
2017-02-02 14:37:42 -0800

    appends content to page with comment and delete buttons

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,16 @@
 $(document).ready(function(){
   // APPEND NEW POST TO PAGE
   $('#publish').click(function(e){
-    var content = $('#newPost').val();
-    console.log(content);
-    var addToPage = '<div class="userPost">' + content + '</div>';
-    console.log(addToPage);
-    $('#allPosts').append(addToPage);
+    let $content = $('#newPost').val();
+    console.log($content);
+    const $addToPage = '<div class="userPost">' + $content + '</div>' + '<div>' + '<button class="comment">comment</button>' + '<button class="delete">delete</button>' + '</div>';
+    console.log($addToPage);
+    $('#allPosts').append($addToPage);
     e.currentTarget.value = " ";
 
     // $.post('/', function(res){
     //   console.log('res =', res);
-    //   const $appendContent = '<div class="userPost">' + $textbox + '</div>';
-    //   $('.allPosts').append($appendContent);
+
     // });
   });
 });
