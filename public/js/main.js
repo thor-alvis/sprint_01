2d62d98ca0552671b5370e8d13698d5e02d763b5
Kristopher Parrillo <kkparrillo@gmail.com>
2017-02-02 13:56:42 -0800

    setting up the routesx

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,24 @@
-//
+console.log('Kris was here...');
+
+// const $loginBtn = $('#login');
+
+// $loginBtn.on('click', (event) => {
+
+// })
+
+
+const $publishBtn = $('#publish');
+const $newPost = $('#newPost').val();
+
+
+$publishBtn.on('click', event => {
+  var data = {
+    username: username,
+    avatar: avatar,
+    post: $newPost
+
+  }
+  $.post('/blog', {data: data}, response => {
+
+  })
+})
