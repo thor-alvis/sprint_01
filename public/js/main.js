129c4e4ee0d7d786eb2b245c279bfb8b7c35231d
Abi Munoz <abimunozr@gmail.com>
2017-02-02 13:52:08 -0800

    appends texbox input to page

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,17 @@
-//
+$(document).ready(function(){
+  // APPEND NEW POST TO PAGE
+  $('#publish').click(function(e){
+    var content = $('#newPost').val();
+    console.log(content);
+    var addToPage = '<div class="userPost">' + content + '</div>';
+    console.log(addToPage);
+    $('#allPosts').append(addToPage);
+    e.currentTarget.value = " ";
+
+    // $.post('/', function(res){
+    //   console.log('res =', res);
+    //   const $appendContent = '<div class="userPost">' + $textbox + '</div>';
+    //   $('.allPosts').append($appendContent);
+    // });
+  });
+});
