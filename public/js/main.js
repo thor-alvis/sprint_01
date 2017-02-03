37df46d5b2e2a98dda51023540922ffe1c0ff120
Abi Munoz <abimunozr@gmail.com>
2017-02-02 18:30:18 -0800

    appends title, textbox contnt and buttons to page

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
   // APPEND NEW POST TO PAGE
   $('#publish').click(function(e){
+    let $title = $('#title').val();
+    console.log($title);
     let $content = $('#newPost').val();
     console.log($content);
-    const $addToPage = `<div class="userPost">${$content}</div>
+    const $addToPage = `<div class="userPost">${$title}<br>${$content}</div>
     <div><button class="comment">comment</button><button class="delete">delete</button><button class="like">like</button></div>`;
     console.log($addToPage);
     $('#allPosts').append($addToPage);
@@ -15,3 +17,22 @@ $(document).ready(function(){
     // });
   });
 });
+
+
+
+
+// $(document).ready(function(){
+//   // APPEND NEW POST TO PAGE
+//   $('#publish').click(function(e){
+//     let $content = $('#newPost').val();
+//     console.log($content);
+//     $.post('/blog', {data: $content} function(res){
+//       console.log('res =', res);
+//       const $addToPage = `<div class="userPost">${$content}</div>
+//       <div><button class="comment">comment</button><button class="delete">delete</button><button class="like">like</button></div>`;
+//       console.log($addToPage);
+//       $('#allPosts').append($addToPage);
+//       e.currentTarget.value = " ";
+//     });
+//   });
+// });
