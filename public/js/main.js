f4a5f2bb87b1147f9803b115675b059b983fb12b
Abi Munoz <abimunozr@gmail.com>
2017-02-02 20:33:21 -0800

    added bootstrap template css

--- public/js/main.js
+++ public/js/main.js
@@ -1,41 +1,22 @@
-$(document).ready(function(){
-  // APPEND NEW POST TO PAGE
-  $('#publish').click(function(e){
-    let $title = $('#title').val();
-    console.log($title);
-    let $content = $('#newPost').val();
-    console.log($content);
-    let $data = {
-      username: null,
-      posts: [{created: Date (),
-         title: $title,
-         content: $content,
-         likes: null
-       }]
-    };
-    console.log($data);
-    $.post('/blogs', {data: $data} function(res){
-      console.log('res =', res);
-      const $addToPage = `<div class="userPost">${$title}<br>${$content}</div>
-      <div><button class="comment">comment</button><button class="delete">delete</button><button class="like">like</button></div>`;
-      console.log($addToPage);
-      $('#allPosts').append($addToPage);
-      e.currentTarget.value = " ";
-    });
-  });
-});
-
-
-
-
 // $(document).ready(function(){
 //   // APPEND NEW POST TO PAGE
 //   $('#publish').click(function(e){
+//     let $title = $('#title').val();
+//     console.log($title);
 //     let $content = $('#newPost').val();
 //     console.log($content);
-//     $.post('/blog', {data: $content} function(res){
+//     let $data = {
+//       username: null,
+//       posts: [{date: null,
+//          title: $title,
+//          content: $content,
+//         //  likes: null
+//        }]
+//     };
+//     console.log($data);
+//     $.post('/blogs', {data: $data} function(res){
 //       console.log('res =', res);
-//       const $addToPage = `<div class="userPost">${$content}</div>
+//       const $addToPage = `<div class="userPost">${$title}<br>${$content}</div>
 //       <div><button class="comment">comment</button><button class="delete">delete</button><button class="like">like</button></div>`;
 //       console.log($addToPage);
 //       $('#allPosts').append($addToPage);
@@ -43,3 +24,22 @@ $(document).ready(function(){
 //     });
 //   });
 // });
+
+
+
+
+$(document).ready(function(){
+  // APPEND NEW POST TO PAGE
+  $('#publish').click(function(e){
+    let $content = $('#newPost').val();
+    console.log($content);
+    $.post('/blog', {data: $content} function(res){
+      console.log('res =', res);
+      const $addToPage = `<div class="userPost">${$content}</div>
+      <div><button class="comment">comment</button><button class="delete">delete</button><button class="like">like</button></div>`;
+      console.log($addToPage);
+      $('#allPosts').append($addToPage);
+      e.currentTarget.value = " ";
+    });
+  });
+});
