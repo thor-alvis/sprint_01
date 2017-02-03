883a5e5bdb7133cd90c57bcffd490799163e5292
brad morgan <bkmorgan3@gmail.com>
2017-02-02 19:33:39 -0800

    updated routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
 
 router.get('/', (req, res, next) => {
   res.render('index', {title: 'Home'})
-})
+});
 
-module.exports = router
+ // show all blogs
+router.get('/blogs', function (req, res, next) {
+  res.send('show all');
+});
+
+// make a new blog
+router.post('/blogs', (req,res)=> {
+});
+
+
+
+
+
+
+module.exports = router;
