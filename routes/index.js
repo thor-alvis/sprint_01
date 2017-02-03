fac854527178e2d735aaa32ef40f3f21b11e35ed
Kristopher Parrillo <kkparrillo@gmail.com>
2017-02-02 19:12:09 -0800

    removed execess

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,6 @@ router.get('/', (req, res, next) => {
   res.render('index', {title: 'Home'})
 })
 
-router.post('/blog/:id', (err, res) => {
-  var username = req.body.username;
-  var post = req.body.post;
-  var data = new UserData
-})
-
 
 
 module.exports = router
